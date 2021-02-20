4aab73b45171ced62b7f44f424b1e325dcb03538
kamimi01 <awakaru0122@gmail.com>
2021-02-21 08:31:48 +0900

    feat: sumOfArrayとasyncSumOfArrayの単体テスト完了

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,81 @@
 // todo: ここに単体テストを書いてみましょう！
+import {
+  sumOfArray,
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+} from "../functions";
+
+/**
+ * sumOfArrayのテスト
+ */
+describe("test of sumOfArray", () => {
+  // 正常系のテスト
+  test("normal case", () => {
+    const testData = [1, 1];
+    const expectedValue: number = 2;
+
+    const receivedValue = sumOfArray(testData);
+
+    expect(receivedValue).toBe(expectedValue);
+  });
+
+  // 空配列を渡して例外を発生させるテスト
+  test("exception case: empty array", () => {
+    const testData: number[] = [];
+
+    expect(() => sumOfArray(testData)).toThrow();
+  });
+
+  // stringの配列を渡すテスト（ビルド時に例外が発生するためテスト不可）
+  // test("exception case: string array", () => {
+  //   const testData: string[] = [];
+
+  //   expect(() => sumOfArray(testData)).toThrow();
+  // });
+
+  // numberを渡すテスト（ビルド時に例外が発生するためテスト不可）
+  // test("exception case: number", () => {
+  //   const testData: number = 1;
+
+  //   expect(() => sumOfArray(testData)).toThrow();
+  // });
+});
+
+/**
+ * asyncSumOfArrayのテスト
+ */
+describe("test of asyncSumOfArray", () => {
+  // 正常系のテスト
+  test("normal case", async () => {
+    const testData = [1, 1];
+    const expectedValue: number = 2;
+
+    // リソースが取得できるまで、待機する
+    const receivedValue = await asyncSumOfArray(testData);
+
+    expect(receivedValue).toBe(expectedValue);
+  });
+
+  // 空配列を渡して例外を発生させるテスト
+  test("exception case: empty array", async () => {
+    const testData: number[] = [];
+
+    expect.assertions(1);
+    await expect(asyncSumOfArray(testData)).rejects.toThrow();
+  });
+});
+
+/**
+ * asyncSumOfArraySometimesZeroのテスト
+ */
+describe("test of asyncSumOfArraySometimesZero", () => {
+  // 正常系のテスト
+  test("normal case", async () => {
+    const testData = [1, 1];
+    const expectedValue: number = 2;
+
+    const receivedValue = await asyncSumOfArraySometimesZero(testData);
+
+    expect(receivedValue).toBe(expectedValue);
+  });
+});
