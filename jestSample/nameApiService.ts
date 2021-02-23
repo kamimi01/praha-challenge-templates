6858ad00b4ad52e1fdc02f0fb63612ae40fe9677
kamimi01 <awakaru0122@gmail.com>
2021-02-24 01:10:02 +0900

    chore: 不要なコードの削除
    #38

--- jestSample/nameApiService.ts
+++ jestSample/nameApiService.ts
@@ -4,19 +4,10 @@ export class NameApiService {
   private MAX_LENGTH = 4;
   public constructor() {}
 
-  // public async getRandomName(): Promise<any> {
-  //   const { data } = await axios.get(
-  //     "https://random-data-api.com/api/name/random_name"
-  //   );
-  //   return { data };
-  // }
-
   public async getFirstName(): Promise<string> {
-    // 依存性オブジェクトを外出しするため、以下のコードを削除
     const { data } = await axios.get(
       "https://random-data-api.com/api/name/random_name"
     );
-    // const { data } = await randomName;
     const firstName = data.first_name as string;
 
     if (firstName.length > this.MAX_LENGTH) {
